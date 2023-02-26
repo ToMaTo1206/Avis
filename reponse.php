3ad808049e2823b54afc2084859cc4731f7d1365
ToMaTo1206 <114519489+ToMaTo1206@users.noreply.github.com>
2023-02-26 16:04:03 +0100

    Update reponse.php

--- reponse.php
+++ reponse.php
@@ -11,21 +11,29 @@ Avis
 </title>
 </head>
 <body>
-    <?php
-    if (isset($_POST['message'])) {
-        $entete  = 'MIME-Version: 1.0' . "\r\n";
-        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $entete .= 'From: webmaster@monsite.fr' . "\r\n";
-        $entete .= 'Reply-to: ' . $_POST['email'];
-
-        $message = '<h1>Message envoyé depuis la page Contact !</h1>
-        <p><b>Email : </b>' . $_POST['email'] . '<br>
-        <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
-
-        $retour = mail('thom84.1206@gmail.com', 'Envoi depuis page Contact', $message, $entete);
-        if($retour)
-            echo '<p>Votre message a bien été envoyé.</p>';
+<?php
+    $destinataire = 'thom84.1206@gmail.com';
+    // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+    $expediteur = $_POST['email'];
+    
+     
+    $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+    $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
+    $headers .= 'To: '.$destinataire."\n"; // Mail de reponse
+    $headers .= 'From: "Nom_de_destinataire"<'.$expediteur.'>'."\n"; // Expediteur
+     
+    $message =  '<div style="width: 100%; text-align: center; font-weight: bold"> Bonjour '.$_POST['name'].'!<br>
+                    '.$_POST['message'].'</div>';
+     
+    if(mail($destinataire, $message, $headers))
+    {
+        echo '<script languag="javascript" >alert("Votre message a bien été envoyé ");</script>';
     }
-    ?>
+    else // Non envoyé
+    {
+        echo '<script languag="javascript">alert("Votre message n\'a pas pu être envoyé");</script>';
+    }
+    header('Location: monformulaire.php');
+?>
 </body>
 </html>
